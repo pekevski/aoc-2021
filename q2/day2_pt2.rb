1eb94c5a0afd20b244993b8a1fd02d73af00dabc
Daniel Pekevski <dpekevski@gmail.com>
2021-12-03 13:43:47 +1100

    Day 2 Submission

---
+++ q2/day2_pt2.rb
@@ -0,0 +1,25 @@
+filename = ARGV.first
+
+file = open(filename)
+
+horizontal_pos = 0
+depth_pos = 0
+aim = 0
+
+file.each_line do |line|
+    instruction, value = line.split
+    value = value.to_i
+
+    case instruction
+    when "forward"
+        horizontal_pos = horizontal_pos + value
+        depth_pos = depth_pos + (aim * value)
+    when "down"
+        aim = aim + value
+    when "up"
+        aim = aim - value
+    end
+end
+
+puts "Horizontal Position: #{horizontal_pos}, Depth Position: #{depth_pos}"
+puts "Answer is: #{horizontal_pos * depth_pos}"
